ef25911a25637072b60ee122aebe0b787a955d20
AldiPutra24 <aldiullum@gmail.com>
2024-07-07 18:48:49 +0700

    fix project card

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
+import ProjectCard2 from "./ProjectCards2";
+import ProjectCard3 from "./ProjectCards3";
 import Particle from "../Particle";
 import editor from "../../Assets/Projects/codeEditor.png";
 import gesturo from "../../Assets/Projects/gesturo.jpg";
 import dataanalyst from "../../Assets/Projects/da.png";
+import ngapotik from "../../Assets/Projects/ngapotik.png";
 
 function Projects() {
   return (
@@ -12,14 +15,14 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="green">Works </strong>
+          My Recent <strong>Works </strong>
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
-            <ProjectCard
+            <ProjectCard3
               imgPath={gesturo}
               isBlog={false}
               title="Gesturo - Sign Learning and Sign Language Translation Application"
@@ -28,8 +31,6 @@ function Projects() {
               demoLink="https://github.com/gesturo-team/Mobile-Development/releases/tag/v1.0.0"
             />
           </Col>
-        </Row>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={editor}
@@ -39,8 +40,6 @@ function Projects() {
               ghLink="https://github.com/AldiPutra24/Timing-Point"
             />
           </Col>
-        </Row>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={dataanalyst}
@@ -49,6 +48,14 @@ function Projects() {
               description="This project aims to analyze the data in the Bike Sharing Dataset with the aim of understanding bike lending patterns, influencing factors, and trends in bike usage over time."
               ghLink="https://github.com/AldiPutra24/Proyek-Analisis-Data_Bike-Sharing.git"
               demoLink="https://aldiputra.streamlit.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard2
+              imgPath={ngapotik}
+              title="UI/UX Design App"
+              description="Ngapotik is an application designed to simplify pharmacy management, providing efficient and user-friendly digital solutions for pharmacy owners, pharmacists, and customers. The project includes user interface (UI) and user experience (UX) designs optimized to provide an intuitive and pleasant usage experience."
+              demoLink="https://www.figma.com/design/UALG79I3QrmT6PPs3pqVxr/Ngapotik-E-Health?node-id=0-1&t=nwgsLmox7WGxdYph-1"
             />
           </Col>
         </Row>
